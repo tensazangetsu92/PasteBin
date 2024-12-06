530db25ea16e896ffdd51a89cf3edeba4db09c27
tensazangetsu92 <manhasnocontrol@gmail.com>
2024-12-07 02:38:53 +0500

    DB connected

---
+++ app/config.py
@@ -0,0 +1,18 @@
+import os
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    @property
+    def DATABASE_URL_asyncpg(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(env_file=os.path.join(os.path.dirname(__file__), '..', '.env'))
+
+settings = Settings()
--- app/database.py
+++ app/database.py
@@ -1,11 +1,22 @@
+import asyncio
+from sqlalchemy import text
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, Mapped
 
-engine = create_async_engine(
-    "sqlite+aiosqlite:///tasks.db",
-    echo=True
-)
-async_session = async_sessionmaker(engine, expire_on_commit=False)
+from .config import settings
 
-class Base(DeclarativeBase):
-    pass
+async_engine = create_async_engine(
+    url=settings.DATABASE_URL_asyncpg,
+    echo=False,
+)
+async def get_123():
+    async with async_engine.connect() as conn:
+        res = await conn.execute(text("SELECT 1,2,3 union select 4,5,6"))
+        print(f"{res.first()=}")
+
+asyncio.run(get_123())
+
+# async_session = async_sessionmaker(engine, expire_on_commit=False)
+#
+# class Base(DeclarativeBase):
+#     pass
